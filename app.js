bda9740ef6e6d5018985e78e8ace39efae067165
Hyona Kim <tekkieninja@gmail.com>
2019-12-21 12:15:55 -0500

    created class objects

--- app.js
+++ app.js
@@ -2,3 +2,29 @@ const   firstName = document.getElementById('first-name'),
         lastName = document.getElementById('last-name'),
         email = document.getElementById('email');
 
+//Email Object
+class EmailContact {
+    constructor(firstName, lastName, email) {
+        this.firstName = firstName,
+        this.lastName = lastName, 
+        this.email = email
+    }
+}
+
+class UI {
+    getInputFields() {
+
+    }
+}
+
+class Store {
+    static addContacttoLocal() {
+
+    }
+}
+
+document.getElementById('email-form-column').addEventListener('submit', function(e) {
+
+    e.preventDefault();
+});
+
